d1fc6da03d5817b0762ffe64a56995c664d35dd0
Fernão Lopes <fernao@riseup.net>
2016-03-16 16:04:44 -0300

    carrossel: colocando link na imagem inteira

--- index.php
+++ index.php
@@ -32,12 +32,12 @@ $posts_destaque = new WP_Query(array('posts_per_page' => '3', 'category__and' =>
                 <?php foreach ($posts_destaque->posts as $post ): ?>
                     <div class="destaque">
     	              <div class="img-responsive">
-                          <img src="<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full'); echo $image[0]; ?>">
+                          <a href="<?php echo get_permalink($post->ID); ?>">
+                            <img src="<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full'); echo $image[0]; ?>">
+			  </a>
                       </div>
     				  <div class="destaque-text">
-                          <a href="<?php echo get_permalink($post->ID); ?>">
-                              <h1><?php echo $post->post_title; ?></h1>
-                          </a>
+                          <h1><?php echo $post->post_title; ?></h1>
 	                  </div>
 
                     </div> <!-- end destaque -->
